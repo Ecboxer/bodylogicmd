dbf339330c746140de33aa421c581c5e2ca20215
eric <ecboxer@gmail.com>
2019-05-06 20:21:50 -0400

    Improving aesthetics

--- vis1_line.R
+++ vis1_line.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(colorspace)
 
 df_app <- read_csv('data/factstablea3.csv')
 df_mat <- read_csv('data/factstablea4.csv')
@@ -11,18 +12,13 @@ df_app %>%
   gather(key=year, value=count, -c('Region')) %>% 
   ggplot() +
   geom_line(aes(x=year, y=count,
-                group=Region, color=Region))
-
-# Number of applicants by state
-df_app %>%
-  filter(`State of Legal Residence`!='Total for the Region') %>% 
-  select(-'% Change') %>% 
-  gather(key=year, value=count,
-         -c('Region', 'State of Legal Residence')) %>% 
-  rename(State='State of Legal Residence') %>% 
-  ggplot() +
-  geom_line(aes(x=year, y=count,
-                group=State, color=State))
+                group=Region, color=Region),
+            size=2) +
+  scale_color_discrete_qualitative(palette='Dark 3') +
+  theme_eric() +
+  ggtitle(label='Medical school applicants by region of legal residence',
+        subtitle='2009-2018') +
+  xlab('') + ylab('Number of matriculants')
 
 # Number of matriculants by region
 df_mat %>%
@@ -32,10 +28,53 @@ df_mat %>%
   gather(key=year, value=count, -c('Region')) %>% 
   ggplot() +
   geom_line(aes(x=year, y=count,
-                group=Region, color=Region))
+                group=Region, color=Region),
+            size=2) +
+  scale_color_discrete_qualitative(palette='Dark 3') +
+  theme_eric() +
+  ggtitle(label='Medical school matriculants by region of legal residence',
+          subtitle='2009-2018') +
+  xlab('') + ylab('Number of matriculants')
 
+# Combine applicants and matriculants
+df1 <- df_app %>%
+  filter(`State of Legal Residence`=='Total for the Region') %>%
+  select(-'State of Legal Residence',
+         -'% Change') %>% 
+  gather(key=year, value=count, -c('Region'))
+df2 <- df_mat %>%
+  filter(`State of Legal Residence`=='Total for the Region') %>%
+  select(-'State of Legal Residence',
+         -'% Change') %>% 
+  gather(key=year, value=count, -c('Region'))
+p1_line <- merge(df1, df2,
+      by=c('year', 'Region'),
+      suffixes=c('_app', '_mat')) %>% 
+  gather(key=key, value=count,
+         -c('year', 'Region')) %>% 
+  mutate(Type=ifelse(key=='count_app',
+                     'Applicants',
+                     'Matriculants')) %>% 
+  ggplot(aes(x=year, y=count,
+             color=Region, linetype=Type,
+             group=interaction(Region, Type))) +
+  geom_line(size=2) + geom_point(size=3) +
+  scale_color_discrete_qualitative(palette='Dark 3') +
+  scale_linetype_manual(values=c('solid',
+                                 'twodash')) +
+  theme_eric() +
+  ggtitle(label='Medical school applicants and matriculants by region of legal residence',
+          subtitle='2009-2018') +
+  xlab('') + ylab('Students')
+p1_line
+# ggsave('vis1_line.svg', plot=p1_line,
+#        device='svg', path='assets',
+#        width=20, height=16)
+
+
+# Scratch: too many states
 # Number of applicants by state
-df_mat %>%
+df_app %>%
   filter(`State of Legal Residence`!='Total for the Region') %>% 
   select(-'% Change') %>% 
   gather(key=year, value=count,
@@ -43,10 +82,18 @@ df_mat %>%
   rename(State='State of Legal Residence') %>% 
   ggplot() +
   geom_line(aes(x=year, y=count,
-                group=State, color=State))
+                group=State, color=State),
+            size=2) +
+  scale_color_discrete_qualitative(palette='Dark 3') +
+  theme_eric()
 
-# TODO include non-states
-# Decide on best metric
-# Neaten
-# Look at A7.2 for sex
-df_mat %>% tail()
+# Number of matriculants by state
+df_mat %>%
+  filter(`State of Legal Residence`!='Total for the Region') %>% 
+  select(-'% Change') %>% 
+p  gather(key=year, value=count,
+         -c('Region', 'State of Legal Residence')) %>% 
+  rename(State='State of Legal Residence') %>% 
+  ggplot() +
+  geom_line(aes(x=year, y=count,
+                group=State, color=State))
--- vis2_sankey_enrollment.R
+++ vis2_sankey_enrollment.R
@@ -17,10 +17,24 @@ for (race in races) {
   nodes <- c(nodes, list('name'=race))
 }
 nodes_list <- nodes %>% unlist(use.names=F)
-
 df_nodes <- nodes_list %>%
   as_data_frame()
 df_nodes <- df_nodes %>%
+  rename(name=value)
+
+# Write nodes df with states
+states <- df_race %>% 
+  filter(State!='Total Enrollment') %>% 
+  select(State) %>%
+  unique() %>% pull()
+states
+for (state in states) {
+  nodes <- c(nodes, list('name'=state))
+}
+nodes_list_states <- nodes %>% unlist(use.names=F)
+df_nodes_states <- nodes_list_states %>% 
+  as_data_frame()
+df_nodes_states <- df_nodes_states %>%
   rename(name=value)
 
 # Create links
@@ -36,8 +50,10 @@ df_sex_enr_sankey <- df_sex_enr %>%
   filter(target!='Total for Men') %>% 
   filter(target!='Total for Women')
 
+df_sex_enr_sankey %>% head()
+
 idx_nodes <- function (node_name) {
-  which(nodes_list==node_name) - 1
+  which(nodes_list_states==node_name) - 1
 }
 df_sex_enr_sankey$source_idx <- df_sex_enr_sankey$source %>%
   lapply(idx_nodes) %>% unlist()
@@ -48,19 +64,65 @@ df_sex_enr_sankey$target_idx <- df_sex_enr_sankey$target %>%
 df_sex_enr_sankey <- df_sex_enr_sankey %>%
   select(-c('source', 'target'))
 
-links <- jsonlite::toJSON(df_sex_enr_sankey)
+# Write race->state links
+cols_race <- colnames(df_race)[-c(1,2)]
+df_race_sankey <- df_race %>%
+  filter(State!='Total Enrollment') %>% 
+  gather(key=race, value=count,
+         -c(State, `Medical School`)) %>% 
+  group_by(State) %>% 
+  mutate(state_count=sum(count)) %>% 
+  ungroup() %>% 
+  select(State, race, state_count) %>% 
+  unique() %>% 
+  mutate(race=str_replace_all(race,
+                              "[\r\n]", ''),
+         race=str_squish(race),
+         race=ifelse(race=='UnduplicatedTotal Enrollment',
+                     'Multiple Race/Ethnicity',
+                     race))
+df_race_sankey$source_idx <- df_race_sankey$race %>% 
+  lapply(idx_nodes) %>% unlist()
+df_race_sankey$target_idx <- df_race_sankey$State %>% 
+  lapply(idx_nodes) %>% unlist()
+df_race_sankey <- df_race_sankey %>% 
+  select(-c(State, race)) %>% 
+  rename(value=state_count)
 
-# Create sankey network
-sankeyNetwork(Links=df_sex_enr_sankey,
+df_sankey_full <- rbind(df_sex_enr_sankey,
+                        df_race_sankey)
+
+# Create sex->race sankey network
+p2_sankey <- sankeyNetwork(Links=df_sex_enr_sankey,
               Nodes=df_nodes,
               Source='source_idx',
               Target='target_idx',
               Value='value',
               NodeID='name',
-              units='enrollees',
+              units='enrolled students',
               fontSize=16,
               nodeWidth=30)
+p2_sankey
 
+# Remove sex from nodes
+df_nodes_states <- df_nodes_states %>% 
+  filter(!name%in%c('Men', 'Women'))
+# Remove sex from links
+df_sankey_states <- df_sankey_full %>% 
+  filter(!source_idx%in%c(0,1)) %>% 
+  mutate(source_idx=source_idx-2,
+         target_idx=target_idx-2)
+
+# Create race->state sankey network
+p2_sankey_st <- sankeyNetwork(Links=df_sankey_states,
+              Nodes=df_nodes_states,
+              Source='source_idx',
+              Target='target_idx',
+              Value='value',
+              NodeID='name',
+              units='enrolled students',
+              fontSize=16,
+              nodeWidth=30)
+p2_sankey_st
 # TODO Neaten
-# Animation?
-# Add state
+# Truncate to just race->region
