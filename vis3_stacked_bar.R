ae06abc5e6ed8f81ff90247c4cf119bb19a17f61
eric <ecboxer@gmail.com>
2019-05-07 10:13:14 -0400

    Cleaning up vis1-3

--- vis3_stacked_bar.R
+++ vis3_stacked_bar.R
@@ -1,10 +1,11 @@
 library(tidyverse)
+library(colorspace)
 
 df_app <- read_csv('data/factstablea10.csv')
 df_mat <- read_csv('data/factstablea11.csv')
 
 # Perc of applicants by state and race
-df_app %>%
+p3_bar <- df_app %>%
   filter(`State of Legal Residence`!='Total for the Region') %>%
   select(-Total) %>% 
   rename(state=`State of Legal Residence`) %>% 
@@ -14,31 +15,28 @@ df_app %>%
   group_by(state) %>% 
   mutate(total_app=sum(app),
          perc_app=app/total_app) %>% 
+  ungroup() %>% 
+  mutate(state=factor(state),
+         state=factor(state,
+                      levels=rev(levels(state)))) %>% 
   ggplot() +
   geom_bar(aes(x=state,
                y=perc_app,
                fill=race),
            stat='identity') +
-  coord_flip()
+  coord_flip() +
+  scale_fill_discrete_qualitative(palette='Dynamic') +
+  theme_eric() +
+  ggtitle(label='Share of medical school applicants by race/ethnicity',
+          subtitle='2018-2019') +
+  labs(fill='Race/Ethnicity') +
+  xlab('') + ylab('Share of applicants')
+p3_bar
+ggsave('vis3_bar.svg', plot=p3_bar,
+       device='svg', path='assets',
+       width=20, height=16)
 
-# Perc of applicants by region and race
-df_app %>%
-  filter(`State of Legal Residence`!='Total for the Region') %>%
-  select(-Total) %>% 
-  rename(state=`State of Legal Residence`) %>% 
-  gather(key=race,
-         value=app,
-         -c(Region, state)) %>% 
-  group_by(Region) %>% 
-  mutate(total_app=sum(app),
-         perc_app=app/total_app) %>% 
-  ggplot() +
-  geom_bar(aes(x=Region,
-               y=perc_app,
-               fill=race),
-           stat='identity') +
-  coord_flip()
-
+# Maybe switch to matriculants?
 # Perc of matriculants by state and race
 df_mat %>%
   filter(`State of Legal Residence`!='Total for the Region') %>%
@@ -57,8 +55,30 @@ df_mat %>%
            stat='identity') +
   coord_flip()
 
-# Perc of matriculants by region and race
+# TODO decide on best metric,
+# is applicant or matriculant used in other assets?
+
+# Scratch regions are not visually interesting
+# Perc of applicants by region and race
 df_app %>%
+  filter(`State of Legal Residence`!='Total for the Region') %>%
+  select(-Total) %>% 
+  rename(state=`State of Legal Residence`) %>% 
+  gather(key=race,
+         value=app,
+         -c(Region, state)) %>% 
+  group_by(Region) %>% 
+  mutate(total_app=sum(app),
+         perc_app=app/total_app) %>% 
+  ggplot() +
+  geom_bar(aes(x=Region,
+               y=perc_app,
+               fill=race),
+           stat='identity') +
+  coord_flip()
+
+# Perc of matriculants by region and race
+df_mat %>%
   filter(`State of Legal Residence`!='Total for the Region') %>%
   select(-Total) %>% 
   rename(state=`State of Legal Residence`) %>% 
@@ -75,7 +95,3 @@ df_app %>%
            stat='identity') +
   coord_flip()
 
-# TODO decide on best metric
-# Neaten 
-# Use perc or total? Potentially misleading for small
-# state with diverse applicants, eg Hawaii
